92c1202cc98f1680d7bff9e56b5c93f8312e89df
Paul Everitt <pauleveritt@me.com>
2019-09-09 17:32:11 -0400

    Automate the tests.

--- src/wired_components/samples/simple/views.py
+++ src/wired_components/samples/simple/views.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from wired.dataclasses import injected, Context
 
-from wired_components.resource import Root
+from wired_components.resource import Root, Collection, Document
 from wired_components.view import view
 
 
@@ -11,3 +11,17 @@ from wired_components.view import view
 class RootView:
     title: str = injected(Context, attr='title')
     template: str = 'rootview.jinja2'
+
+
+@view(context=Collection)
+@dataclass
+class CollectionView:
+    title: str = injected(Context, attr='title')
+    template: str = 'collectionview.jinja2'
+
+
+@view(context=Document)
+@dataclass
+class DocumentView:
+    title: str = injected(Context, attr='title')
+    template: str = 'documentview.jinja2'
--- tests/samples/simple/test_views.py
+++ tests/samples/simple/test_views.py
@@ -8,13 +8,21 @@ def app(registry) -> None:
     wired_setup(registry)
 
 
-def test_homepage(registry, app):
+@pytest.mark.parametrize(
+    'path, heading',
+    [
+        ('/', 'Root: My Site'),
+        ('/f1', 'Folder: The Folder At The Root'),
+        ('/d1', 'Document: A Doc At The Root'),
+    ]
+)
+def test_pages(registry, app, path, heading):
     from wired_components.samples.simple import render_path
-    result = render_path(registry, '/')
+    result = render_path(registry, path)
 
     # Parse it and do some tests
     soup: BeautifulSoup = BeautifulSoup(result, 'html.parser')
     nav = soup.find('nav').string.strip()
     assert nav == 'BC: label is BC'
     h1 = soup.find('h1').string.strip()
-    assert h1 == 'Root: My Site'
+    assert h1 == heading
