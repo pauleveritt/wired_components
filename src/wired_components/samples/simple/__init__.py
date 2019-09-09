1e2cd39387fd94e70aadc3b1a61a46ae53acd25b
Paul Everitt <pauleveritt@me.com>
2019-09-09 17:26:04 -0400

    Move the logic from test to sample app.

--- src/wired_components/samples/simple/__init__.py
+++ src/wired_components/samples/simple/__init__.py
@@ -12,15 +12,19 @@ Not doing anything exotic (e.g. strictyaml, pydantic.)
 """
 from pathlib import Path
 
-from wired import ServiceRegistry
+from markupsafe import Markup
+from wired import ServiceRegistry, ServiceContainer
 
 from wired_components import wired_setup as global_setup
 from wired_components.configuration import IConfiguration, Configuration
-from wired_components.resource import Root, IRoot
-from wired_components.scanner import WiredScanner, IScanner
-
+from wired_components.renderer import JinjaRenderer, IJinjaRenderer
+from wired_components.request import find_resource
+from wired_components.resource import Root, IRoot, Resource
 from wired_components.samples.simple import components, views
+from wired_components.scanner import WiredScanner, IScanner
+from wired_components.view import View, IView, as_dict
 from .loader import load_resources, load_yaml
+
 
 def root_setup(registry: ServiceRegistry) -> None:
     from wired_components import samples
@@ -51,11 +55,31 @@ def wired_setup(registry: ServiceRegistry):
     scanner.scan(components)
     scanner.scan(views)
 
-    from wired_components.component import register_component
-    register_component(registry, components.Breadcrumb)
+
+def render_path(registry: ServiceRegistry, resource_path: str) -> str:
+    """ Render a resource at a path, to a string """
+
+    # Get the root, then the context at the path
+    container1: ServiceContainer = registry.create_container()
+    root: Root = container1.get(IRoot)
+    context: Resource = find_resource(root, resource_path)
+
+    # Now make a container to process this context
+    container2: ServiceContainer = registry.create_container(context=context)
+    view: View = container2.get(IView)
+
+    renderer: JinjaRenderer = container2.get(IJinjaRenderer)
+    context_dict = as_dict(view)
+    template_name = view.template
+    markup: Markup = renderer.render(
+        context_dict, template_name=template_name, container=container2
+    )
+
+    return str(markup)
 
 
 __all__ = [
     'load_resources',
     'load_yaml',
+    'render_path',
 ]
--- tests/samples/simple/test_views.py
+++ tests/samples/simple/test_views.py
@@ -1,6 +1,5 @@
 import pytest
 from bs4 import BeautifulSoup
-from markupsafe import Markup
 
 
 @pytest.fixture
@@ -9,23 +8,12 @@ def app(registry) -> None:
     wired_setup(registry)
 
 
-def test_homepage(registry, app, simple_root):
-    from wired_components.renderer import IJinjaRenderer, JinjaRenderer
-    from wired_components.view import IView, View, as_dict
+def test_homepage(registry, app):
+    from wired_components.samples.simple import render_path
+    result = render_path(registry, '/')
 
-    context = simple_root
-    container = registry.create_container(context=context)
-
-    # Get the view for root
-    view: View = container.get(IView)
-
-    renderer: JinjaRenderer = container.get(IJinjaRenderer)
-    context = as_dict(view)
-    template_name = view.template
-    markup: Markup = renderer.render(
-        context, template_name=template_name, container=container
-    )
-    soup: BeautifulSoup = BeautifulSoup(markup, 'html.parser')
+    # Parse it and do some tests
+    soup: BeautifulSoup = BeautifulSoup(result, 'html.parser')
     nav = soup.find('nav').string.strip()
     assert nav == 'BC: label is BC'
     h1 = soup.find('h1').string.strip()
